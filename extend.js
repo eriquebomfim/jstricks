6ccdca44b2b9d9c4c19e2d0c8c412837b178f349
Erique Bomfim @ MP <erique.bomfim@mpba.mp.br>
2022-01-18 11:04:42 -0300

    Extends is a function that helps to merge two javascript object updating existing properties, adding new ones

---
+++ extend.js
@@ -0,0 +1,47 @@
+
+/**
+* Extend provides a way to merge two objects
+* or update property value from original object
+* @target <Object> - original object
+* @ext    <Object> - object to merge with
+* @root   <String> - the very root property
+*/
+
+extend = function(target, ext, root = null){
+    return Object.keys(target).reduce((rdx,curr)=>{ 
+        if (ext[curr]){
+            if (ext[curr].constructor.name == "Object"){ 
+                rdx[curr] = extend(target[curr], ext[curr]); 
+            } else {
+                rdx[curr] = ext[curr];                                
+            }
+        } else {
+            rdx[curr] = target[curr];
+        }
+        return rdx;
+    },ext);  
+}
+
+/**
+ * The original Object
+ */
+var a = {
+    "car":{
+        "wheels":4,
+        "brand" :"GM",
+        "tint"  :{
+            "type":"metalic"
+        }
+    }
+}
+
+z = extend(a,{
+    "car":{
+        "tint":{
+            "type" : "solid",
+            "color": "blue"
+        }
+    }
+});
+
+console.dir(z);
